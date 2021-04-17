121f04eb3c3c0425b4064eec75d83b6fe1bd557d
Jeff Corbett <jcorbett@dejotech.com>
2021-04-17 19:56:58 -0400

    Updated array_while_for.js for loop to find and display the scores.length and highScore

--- array_while_for.js
+++ array_while_for.js
@@ -11,9 +11,15 @@ while (i < scores.length) {
 
 // array iteration with for loop
 let scores1 = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+let highScore = 0;
 let output1;
 
 for (let i = 0; i < scores1.length; i++) {
     output1 = "Bubble solution #" + i + " score: " + scores1[i];
     console.log(output1);
+    if (scores1[i] > highScore) {
+        highScore = scores1[i];
+    }
 }
+console.log("Bubbles test: " + scores.length);
+console.log("Highest bubble score: " + highScore);
