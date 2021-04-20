1fe2d7f44d1877050d510e70071b02200196b4a1
DeJoTech <jcorbett@dejotech.com>
2021-04-19 21:06:09 -0400

    Added cars.js for very basic object examples

---
+++ cars.js
@@ -0,0 +1,31 @@
+let chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+};
+
+let cadillac = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: "5",
+    convertible: false,
+    mileage: 12892
+};
+
+let fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000
+};
+
+console.log(fiat.year);
