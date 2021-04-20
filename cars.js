51758c1049742a15402cb78e112e15c4e93435e0
DeJoTech <jcorbett@dejotech.com>
2021-04-20 14:22:33 -0400

    Updated cars.js to continue basic object examples

--- cars.js
+++ cars.js
@@ -28,4 +28,29 @@ let fiat = {
     mileage: 88000
 };
 
-console.log(fiat.year);
+let taxi = {
+    make: "Webville Motors",
+    model: "Taxi",
+    year: 1955,
+    color: "yellow",
+    passengers: 4,
+    convertible: false,
+    mileage: 281341
+}
+
+function preQualify(car) {
+    if (car.mileage > 10000) {
+        return false;
+    } else if (car.year > 1960) {
+        return false;
+    }
+    return true;
+}
+
+let worthALook = preQualify(taxi);
+
+if (worthALook) {
+    console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+    console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+}
