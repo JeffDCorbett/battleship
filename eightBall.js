f835ed7a13cf9fafcf8dc7a54bc4cd2a364d78e4
DeJoTech <jcorbett@dejotech.com>
2021-04-21 18:17:26 -0400

    Added eightBall.js to get more object - methods practice

---
+++ eightBall.js
@@ -0,0 +1,16 @@
+let eightBall = {
+    index: 0,
+    advice: ["Absolutely", "HA HA HA! Yeah Right!", "Potentially, but don't get your hopes up.", "Ever heard the song, Dream On?"],
+    shake: function () {
+        this.index = this.index + 1;
+        if (this.index >= this.advice.length) {
+            this.index = 0;
+        }
+    },
+    look: function () {
+        return this.advice[this.index];
+    }
+};
+
+eightBall.shake();
+console.log(eightBall.look());
