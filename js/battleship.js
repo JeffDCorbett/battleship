e53ec61d4b033b768c90a666e738685ff03894f0
Jeff Corbett <jcorbett@dejotech.com>
2021-04-28 11:02:01 -0400

    Added model object and test commands.

--- js/battleship.js
+++ js/battleship.js
@@ -13,11 +13,62 @@ let view = {
     }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
+// commands to test the view object
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
+//
+// view.displayMessage("Tap Tap, is this thing on?..")
 
-view.displayMessage("Tap Tap, is this thing on?..")
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLength: 3,
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""] },
+        {locations: ["24", "34", "44"], hits: ["", "", ""] },
+        {locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+
+    fire: function (guess){
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my Battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMessage(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function (ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// code to test the model object
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
